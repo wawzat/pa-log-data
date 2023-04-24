6d1ba2e5771a50b9579fadbf3b43b7c526157f4f
wawzat <wawzat@gmail.com>
2023-04-24 11:15:48 -0700

    Testing process_data()

--- pa_log_data.py
+++ pa_log_data.py
@@ -192,16 +192,17 @@ def process_data(document_name, client):
         )
         #df.set_index('time_stamp', inplace=True)
         df = df.set_index('time_stamp')
-        # Humidity, temperature and pressure are in the RS dataframe at this point
-        df = df.dropna(subset=['humidity', 'temperature', 'pressure'])
-        df = df.fillna('')
-        df_summarized = df.groupby('name').resample('2H').mean(numeric_only=True)
-        # Above is where the humidity, temperature and pressure data are lost
         pd.set_option('display.max_columns', None)
         print(" ")
         print(k)
         print(" ")
-        print(df_summarized)
+        print(df)
+        # Humidity, temperature and pressure are in the RS dataframe at this point
+        #df = df.dropna(subset=['humidity', 'temperature', 'pressure'])
+        #df = df.fillna('')
+        df_summarized = df.groupby('name').resample('2H').mean(numeric_only=True)
+        # Above is where the humidity, temperature and pressure data are lost
+
         print(" ")
         print(df_summarized[['humidity', 'temperature', 'pressure']])
         print(" ")
