7b3700e302eae7475fcfd91585a5d77193964520
wawzat <wawzat@gmail.com>
2023-06-01 15:49:01 -0700

    First commit

---
+++ gsheets_mgmt.py
@@ -0,0 +1,68 @@
+"""
+This program lists all the Google Sheets files associated with a given service account and optionally deletes them. 
+It requires a JSON key file for the service account and the `gspread` and `oauth2client` libraries to be installed.
+
+Usage:
+    python gsheets_mgmt.py [-d]
+
+Arguments:
+    -d, --delete: Use this argument to delete files. If not specified, only the list of files will be printed.
+
+Example:
+    python gsheets_mgmt.py -d
+
+This will list all the Google Sheets files associated (owned or shared) with the service account and optionally delete 
+all the files owned by the Service Account with 'history' in their name, except for those with '401K' or 'data' in their name.
+"""
+# James S. Lucas 20230601
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import argparse
+import config
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+    description='List and optionally delete files from Google Sheets ServiceAccount.',
+    prog='gsheets_mgmt.py',
+    usage='%(prog)s [-d <delete>]',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    g=parser.add_argument_group(title='arguments',
+            description='''    -d, --delete  Use this argument to delete files.      ''')
+    g.add_argument('-d', '--delete', action='store_true',
+                    dest='delete',
+                    help=argparse.SUPPRESS)
+
+    args = parser.parse_args()
+    return(args)
+args = get_arguments()
+
+
+# set the credentials for the Google Sheets service account
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'
+        ]
+creds = ServiceAccountCredentials.from_json_keyfile_name(config.GSPREAD_SERVICE_ACCOUNT_JSON_PATH, scope)
+client = gspread.authorize(creds)
+
+spreadsheets = client.list_spreadsheet_files()
+
+print()
+print('All Files:')
+for spreadsheet in spreadsheets:
+    message = f"Spreadsheet ID: {spreadsheet['id']}, Name: {spreadsheet['name']}"
+    print(message)
+print()
+print('History Files:')
+for spreadsheet in spreadsheets:
+    if 'history' in spreadsheet['name']:
+        message = f"Spreadsheet ID: {spreadsheet['id']}, Name: {spreadsheet['name']}"
+        print(message)
+        if args.delete is True:
+            if '401K' not in spreadsheet['name'] or 'data' not in spreadsheet['name']:
+                try:
+                    client.del_spreadsheet(spreadsheet['id'])
+                except gspread.exceptions.APIError as e:
+                    if 'insufficientFilePermissions' in str(e):
+                        pass
+print()
