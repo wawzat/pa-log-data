1ac11692fcfa3c3c95dad4c78785816aaabfa80a
wawzat <wawzat@gmail.com>
2023-06-12 11:14:08 -0700

    Add Google personal email

--- config_template.py
+++ config_template.py
@@ -5,3 +5,6 @@ PURPLEAIR_WRITE_KEY = ''
 #Google credentials for gspread to access Google Sheets
 GSPREAD_SERVICE_ACCOUNT_JSON_PATH = r'C:\Users\username\AppData\Roaming\gspread\service_account.json'
 SERVICE_ACCOUNT_USER_EMAIL = 'username@project_name.iam.gserviceaccount.com'
+
+# Personal Google account to share Google Sheets workbook with
+google_account = 'user@gmail.com'
