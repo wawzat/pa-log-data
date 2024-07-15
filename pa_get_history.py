3bb7d24eeb7f46d80c1f55f2c3da2df221fe7dc0
wawzat <wawzat@gmail.com>
2024-07-15 15:32:01 -0700

    Comments

--- pa_get_history.py
+++ pa_get_history.py
@@ -130,7 +130,7 @@ def get_arguments():
     g=parser.add_argument_group(title='arguments',
             description='''            -m, --month      Optional. The month to get data for. If not provided, current month will be used.
             -y, --year       Optional. The year to get data for. If not provided, current year will be used.
-            -d, --directory  Optional. A directory suffix to append to the default directory name. Default YYYY-MM.
+            -d, --directory  Optional. A directory suffix to append to the default directory name. An underscore is automatically prefixed. Default YYYY-MM.
             -s, --sensor     Optional. Sensor Name. If not provided, constants.py sensors_current will be used.
             -o, --output     Optional. Output format. Default is CSV file. CSV, Google Sheets, XL, All. Choices = c, s, x, a 
             -a, --average    Optional. Number of minutes to average. If not provided, 30 minutes will be used. Choices = 0, 10, 30, 60, 360, 1440
